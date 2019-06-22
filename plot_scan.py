a1646b01fe4f06f7857a020026198c80aa9336c4
Andrea Bocci <andrea.bocci@cern.ch>
2019-06-22 12:24:31 +0200

    Add options for the X and Y values

--- plot_scan.py
+++ plot_scan.py
@@ -8,6 +8,12 @@ import pandas as pd
 import matplotlib as mpl
 import seaborn as sns
 
+# plot content options
+options = {
+  'normalise':  False,              # True: plot the average throughput per job, False: plot the total throughput
+  'x axis':    'EDM streams',       # 'CPU threads per job', 'CPU threads', 'EDM streams per job', 'EDM streams'
+}
+
 # workaround for seaborn 0.9.0
 def fix_plot_range(plot):
   data = plot.data[plot._x_var]
@@ -18,9 +24,9 @@ def fix_plot_range(plot):
   plot.set(ylim=(0, None))
 
 
-sns.set(style={                 # based on 'whitegrid'
+sns.set(style={                     # based on 'whitegrid'
   'axes.axisbelow': True,
-  'axes.edgecolor': '.15',      # .8
+  'axes.edgecolor': '.15',          # .8
   'axes.facecolor': 'white',
   'axes.grid': True,
   'axes.labelcolor': '.15',
@@ -47,11 +53,11 @@ sns.set(style={                 # based on 'whitegrid'
 })
 
 sns.set_palette([
-  (0.,  0.,  1.),           # ROOT kBlue
-  (1.,  0.,  0.),           # ROOT kRed
-  (0.,  0.,  0.),           # ROOT kBlack
-  (1.,  0.4, 0.),           # ROOT kOrange +7
-  (0.8, 0.2, 0.8),          # ROOT kMagenta -3
+  (0.,  0.,  1.),                   # ROOT kBlue
+  (1.,  0.,  0.),                   # ROOT kRed
+  (0.,  0.,  0.),                   # ROOT kBlack
+  (1.,  0.4, 0.),                   # ROOT kOrange +7
+  (0.8, 0.2, 0.8),                  # ROOT kMagenta -3
 ], 5)
 
 data = []
@@ -76,13 +82,19 @@ df = pd.concat(data, ignore_index = True)
 del data
 
 # normalise to the number of jobs
-df['average throughput (ev/s)'] /= df['jobs']
-df['uncertainty (ev/s)']        /= df['jobs']
+if options['normalise']:
+  df['average throughput (ev/s)'] /= df['jobs']
+  df['uncertainty (ev/s)']        /= df['jobs']
+
+# compute the total numer of CPU threads and EDM streams
+df['CPU threads'] =  df['CPU threads per job'] * df['jobs']
+df['EDM streams'] =  df['EDM streams per job'] * df['jobs']
 
 plot = sns.lmplot(
   data = df,
-  x = 'CPU threads per job',
+  x = options['x axis'],
   y = 'average throughput (ev/s)',
+  fit_reg = True,                   # estimate and plot a regression model
   order = 4,                        # polynomial fit
   hue = 'name',                     # different categories
   height = 5.4,                     # plot height in inches, at 100 dpi
