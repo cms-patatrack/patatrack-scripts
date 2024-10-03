44062b46fa689fbb636e046040374d5ff5b7a5e2
Andrea Bocci <andrea.bocci@cern.ch>
2024-10-03 08:52:54 +0200

    Add initial support for AMD GPUs (#31)

--- gpuinfo.py
+++ gpuinfo.py
@@ -14,45 +14,70 @@ class GPUInfo(object):
 
 
 # cache results across calls
-__cache = None
+__cache_amd = None
+__cache_nvidia = None
 
 
 # return a mapping between devices and GPUInfo objects
-def get_gpu_info(cache = True):
-  global __cache
-  if cache and __cache:
-    return __cache
+def get_gpu_info_amd(cache = True):
+  global __cache_amd
+  if cache and __cache_amd:
+    return __cache_amd
 
   gpus = collections.OrderedDict()
 
-  visible = None
-  if 'CUDA_VISIBLE_DEVICES' in os.environ:
-    if os.environ['CUDA_VISIBLE_DEVICES'] == '':
-      visible = []
-    else:
-      visible = [int(device) for device in os.environ['CUDA_VISIBLE_DEVICES'].split(',')]
+  devices = subprocess.Popen(['rocmComputeCapabilities', ], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True).communicate()[0]
+  for line in devices.splitlines():
+    matches = re.match(r' *([0-9]+) +(gfx[0-9]+) +(.*)', line)
+    if matches:
+      device = int(matches.group(1))
+      model  = matches.group(3).strip()
+      gpus[device] = GPUInfo(device, model)
+
+  if cache:
+    __cache_amd = gpus
+
+  return gpus
+
+
+def get_gpu_info_nvidia(cache = True):
+  global __cache_nvidia
+  if cache and __cache_nvidia:
+    return __cache_nvidia
+
+  gpus = collections.OrderedDict()
 
   devices = subprocess.Popen(['cudaComputeCapabilities', ], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True).communicate()[0]
   for line in devices.splitlines():
     matches = re.match(r' *([0-9]+) +([0-9]+\.[0-9]) +(.*)', line)
     if matches:
       device = int(matches.group(1))
-      if visible:
-        device = visible[device]
       model  = matches.group(3).strip()
       gpus[device] = GPUInfo(device, model)
 
   if cache:
-    __cache = gpus
+    __cache_nvidia = gpus
 
   return gpus
 
 
 if __name__ == "__main__":
-  gpus = get_gpu_info()
+  # NVIDIA GPUs
+  gpus = get_gpu_info_nvidia()
   if gpus:
-    print('%d visible NVIDIA GPUs:' % len(gpus))
+    print('%d visible NVIDIA CUDA GPUs:' % len(gpus))
     for gpu in gpus.values():
       print('  %d: %s' % (gpu.device, gpu.model))
   else:
-    print('No visible NVIDIA GPUs')
+    print('No visible NVIDIA CUDA GPUs')
+  print()
+  
+  # AMD GPUs
+  gpus = get_gpu_info_amd()
+  if gpus:
+    print('%d visible AMD ROCm GPUs:' % len(gpus))
+    for gpu in gpus.values():
+      print('  %d: %s' % (gpu.device, gpu.model))
+  else:
+    print('No visible AMD ROCm GPUs')
+  print()
