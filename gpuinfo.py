df2824aa814e2764322cea3d1ec82304aa9f02a2
Andrea Bocci <andrea.bocci@cern.ch>
2024-10-06 23:05:50 +0000

    Support more AMD GPUs (#32)

--- gpuinfo.py
+++ gpuinfo.py
@@ -8,9 +8,10 @@ import collections
 
 
 class GPUInfo(object):
-  def __init__(self, device = None, model = None):
+  def __init__(self, device = None, model = None, arch = None):
     self.device = device
     self.model  = model
+    self.arch   = arch
 
 
 # cache results across calls
@@ -28,11 +29,12 @@ def get_gpu_info_amd(cache = True):
 
   devices = subprocess.Popen(['rocmComputeCapabilities', ], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True).communicate()[0]
   for line in devices.splitlines():
-    matches = re.match(r' *([0-9]+) +(gfx[0-9]+) +(.*)', line)
+    matches = re.match(r' *([0-9]+) +(gfx[0-9]+(:(sramecc|xnack)[+-])*) +(.*)', line)
     if matches:
       device = int(matches.group(1))
-      model  = matches.group(3).strip()
-      gpus[device] = GPUInfo(device, model)
+      model  = matches.group(5).strip()
+      arch   = matches.group(2).strip()
+      gpus[device] = GPUInfo(device, model, arch)
 
   if cache:
     __cache_amd = gpus
@@ -53,7 +55,10 @@ def get_gpu_info_nvidia(cache = True):
     if matches:
       device = int(matches.group(1))
       model  = matches.group(3).strip()
-      gpus[device] = GPUInfo(device, model)
+      arch   = matches.group(2).strip()
+      # remap 7.5 to sm_75
+      arch   = 'sm_' + arch.replace('.', '')
+      gpus[device] = GPUInfo(device, model, arch)
 
   if cache:
     __cache_nvidia = gpus
@@ -67,7 +72,7 @@ if __name__ == "__main__":
   if gpus:
     print('%d visible NVIDIA CUDA GPUs:' % len(gpus))
     for gpu in gpus.values():
-      print('  %d: %s' % (gpu.device, gpu.model))
+      print('  %d: %s (%s)' % (gpu.device, gpu.model, gpu.arch))
   else:
     print('No visible NVIDIA CUDA GPUs')
   print()
@@ -77,7 +82,7 @@ if __name__ == "__main__":
   if gpus:
     print('%d visible AMD ROCm GPUs:' % len(gpus))
     for gpu in gpus.values():
-      print('  %d: %s' % (gpu.device, gpu.model))
+      print('  %d: %s (%s)' % (gpu.device, gpu.model, gpu.arch))
   else:
     print('No visible AMD ROCm GPUs')
   print()
