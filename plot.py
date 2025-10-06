07ddec87eeb5471ad4276ce6a44059462ea1369d
Bruno Alves <balves16@hotmail.com>
2025-10-06 11:28:43 +0200

    Compactify argparse options naming.

--- plot.py
+++ plot.py
@@ -6,7 +6,7 @@ Plot average throughput vs number of CPU threads per job.
 Command examples:
 
 + Read 3 csv files and store OUTPUT.pdf and OUTPUT.png:
-python3 patatrack-scipts/plot.py scan/reduced_hlt_{ecal,hcal,pixel}_w7900.csv --title Labels --csv_labels ECAL HCAL Pixel -o OUTPUT
+python3 patatrack-scipts/plot.py scan/reduced_hlt_{ecal,hcal,pixel}_w7900.csv --title Labels --labels ECAL HCAL Pixel -o OUTPUT
 """
 
 import pandas as pd
@@ -27,11 +27,11 @@ parser.add_argument('-o', '--output', type=str, default="throughput_vs_threads",
                     help="Base filename for output files (PNG/PDF)")
 parser.add_argument('-x', '--x-axis', default='CPU threads per job',
                     help='Horizontal axis label.')
-parser.add_argument('--csv_labels', nargs='+', default=None, required=False,
-                    help="CSV labels to process (one-to-one mapping with the CSV files)")
+parser.add_argument('--labels', nargs='+', default=None,
+                    help="Labels to show in the legend instead of the CSV file names")
 
 # Positional arguments (CSV files)
-parser.add_argument('csv_files', nargs='+', help="CSV files to process")
+parser.add_argument('files', nargs='+', help="CSV files to process")
 
 # Parse arguments
 args = parser.parse_args()
@@ -39,15 +39,15 @@ args = parser.parse_args()
 # Access the values
 title = args.title
 filename = args.output
-csv_files = args.csv_files
-csv_labels = args.csv_labels
-if csv_labels is not None:
-    assert len(csv_files) == len(csv_labels)
+files = args.files
+labels = args.labels
+if labels is not None:
+    assert len(files) == len(labels)
 
 # Dictionary to store per-file datasets
 datasets = {}
 
-for file in csv_files:
+for file in files:
     # Read CSV and clean column names
     df = pd.read_csv(file)
     df.columns = df.columns.str.strip()
@@ -64,7 +64,7 @@ for file in csv_files:
     )
     
     # Create a nicer label: remove extension and replace underscores
-    label = os.path.basename(file) if csv_labels is None else csv_labels[csv_files.index(file)]
+    label = os.path.basename(file) if labels is None else labels[files.index(file)]
     if label.endswith(".csv"):
         label = label[:-4]
     label = label.replace("_", " ")
