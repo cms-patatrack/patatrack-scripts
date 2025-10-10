6aa3f8c67af5982d99de5c720edf84506d238de4
Bruno Alves <balves16@hotmail.com>
2025-10-10 07:55:28 +0200

    Remove unspecified slot arguments from print.

--- slot.py
+++ slot.py
@@ -141,7 +141,7 @@ class Slot:
         self.amd_gpus = Slot.parse_gpu_descriptor(amd_gpus)
 
     def __str__(self):
-        return ', '.join([f'{k}={v}' for k,v in vars(self).items()])
+        return ', '.join([f'{k}={v}' for k,v in vars(self).items() if v is not None])
     
     # return "value" if "field=value" is given in arg, or None if field is not in arg
     @staticmethod
