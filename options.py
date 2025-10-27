b5a5ded7c79311f9bc3eef7f736714bd2e427809
Bruno Alves <balves16@hotmail.com>
2025-10-27 16:51:53 +0100

    Implement __str__ for Slot (#47)

--- options.py
+++ options.py
@@ -8,7 +8,7 @@ def printCommonArgs(opts):
         if key == 'slots':
             print(f'  --{key}')
             for idx, val in enumerate(value):
-                print(f'      slot {idx}: ' + ', '.join([f'{k}={v}' for x in value for k,v in vars(x).items()]))
+                print(f'      slot {idx}: {val}')
         else:
             print(f'  --{key}: {value}')
     print()
